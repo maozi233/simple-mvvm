1330aa19494f64c8401de3332e1feb6390b61fc5
maoziwocao <623697537@qq.com>
2020-06-07 15:38:26 +0800

    feat(dep): 发布订阅文件

---
+++ src/dep.js
@@ -0,0 +1,32 @@
+
+export class Dep {
+  constructor() {
+    // 存放依赖(watcher 每个属性都会有一个watcher  )
+    this.deps = [];
+  }
+
+  addDep(dep) {
+    this.deps.push(dep)
+  }
+
+  notify() {
+    this.deps.forEach(dep => dep.update())
+  }
+}
+
+export class Watcher {
+  constructor(vm, key, callback) {
+    this.vm = vm;
+    this.key = key;
+    this.callback = callback;
+    // 把当前属性的watcher挂载到Dep的静态属性target下
+    Dep.target = this;
+    this.vm[this.key]; // 触发defineReactive下当前属性（key）的getter
+    Dep.target = null;
+  }
+
+  update() {
+    // console.log('属性更新了')
+    this.callback.call(this.vm, this.vm[this.key]);
+  }
+}
